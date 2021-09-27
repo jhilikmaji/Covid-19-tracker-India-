6f018b376f68248559d2ae094680bdb2ba4a2be6
Jhilik Maji <jhilik.maji98@gmail.com>
2021-09-27 15:25:25 +0530

    logic

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 $(document).ready(function(){
     var url="https://data.covid19india.org/data.json"
     $.get(url,function(data){
-        console.log(data)
+       
         var totalActive,totalRecovered,totalDeaths,totalConfirmed
-        var state = []
-        var confirmed = []
-        var recovered = []
-        var deaths = []
-        $.each(data.statewise,function(id,obj){
-            state.push(obj.state)
-            confirmed.push(obj.confirmed)
-            recovered.push(obj.recovered)
-            deaths.push(obj.deaths)
-
-        })
-        console.log(state)
-        state.shift()
-        confirmed.shift()
-        recovered.shift()
-        deaths.shift()
         totalActive=data.statewise[0].active
         totalRecovered=data.statewise[0].recovered
         totalDeaths=data.statewise[0].deaths
@@ -28,6 +12,21 @@ $(document).ready(function(){
         $("#deaths").append(totalDeaths)
         $("#confirmed").append(totalConfirmed)
 
+        var state = []
+        var confirmed = []
+        var recovered = []
+        var deaths = []
+        $.each(data.statewise,function(id,obj){
+            if(id!=0){
+                state.push(obj.state)
+                confirmed.push(obj.confirmed)
+                recovered.push(obj.recovered)
+                deaths.push(obj.deaths)
+    
+            }
+          
+        })
+        
         var myChart=document.getElementById("myChart").getContext("2d")
         var chart=new Chart(myChart,{
             type:'line',
@@ -38,19 +37,19 @@ $(document).ready(function(){
                     label:"Confirmed Cases",
                     data:confirmed,
                     backgroundColor:"#ffff66",
-                    
+                    borderColor:"#ffff66"
                     },
                     {
                         label:"Recovered Cases",
                         data:recovered,
                         backgroundColor:"#9fff80",
-                        
+                        borderColor:"#9fff80"
                     },
                     {
                         label:"Death Cases",
                         data:deaths,
                         backgroundColor:"#ff8080",
-                        
+                        borderColor:"#ff8080"
                     },
                 ]
             },
