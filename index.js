19967b4cd077772a218a09637cf7853698096d8d
Jhilik Maji <jhilik.maji98@gmail.com>
2021-10-02 23:06:11 +0530

    new change

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
 $(document).ready(function(){
-    var url="https://data.covid19india.org/data.json"
+    var url="https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true"
     $.get(url,function(data){
-       
-        var totalActive,totalRecovered,totalDeaths,totalConfirmed
-        totalActive=data.statewise[0].active
-        totalRecovered=data.statewise[0].recovered
-        totalDeaths=data.statewise[0].deaths
-        totalConfirmed=data.statewise[0].confirmed
-        $("#active").html(totalActive)
-        $("#recovered").html(totalRecovered)
-        $("#deaths").html(totalDeaths)
-        $("#confirmed").html(totalConfirmed)
-
-        var state = []
-        var confirmed = []
-        var recovered = []
-        var deaths = []
-        $.each(data.statewise,function(id,obj){
-            if(id!=0){
-                state.push(obj.state)
-                confirmed.push(obj.confirmed)
-                recovered.push(obj.recovered)
-                deaths.push(obj.deaths)
+      
+       $("#confirmed").html(data.totalCases)
+       $("#active").html(data.activeCases)
+       $("#recovered").html(data.recovered)
+       $("#deaths").html(data.deaths)
+    var region=[]
+    var activeCases=[]
+    var recovered=[]
+    var deceased=[]
     
-            }
-          
-        })
-        
-        var myChart=document.getElementById("myChart").getContext("2d")
-        var chart= new Chart(myChart,{
-            type:'line',
-            data:{
-                labels:state,
-                datasets:[
-                    {
-                    label:"Confirmed Cases",
-                    data:confirmed,
-                    backgroundColor:"#ffff66",
-                    borderColor:"#ffff66"
-                    },
-                    {
-                        label:"Recovered Cases",
-                        data:recovered,
-                        backgroundColor:"#9fff80",
-                        borderColor:"#9fff80"
-                    },
-                    {
-                        label:"Death Cases",
-                        data:deaths,
-                        backgroundColor:"#ff8080",
-                        borderColor:"#ff8080"
-                    },
-                ]
-            },
-            options:{}
-        })
+    $.each(data.regionData,function(id,obj){
+        region.push(obj.region)
+        activeCases.push(obj.totalInfected)
+        recovered.push(obj.recovered)
+        deceased.push(obj.deceased)
+       
     })
+    
+      
+    var myChart=document.getElementById("myChart").getContext("2d")
+    var chart= new Chart(myChart,{
+        type:'line',
+        data:{
+            labels:region,
+            datasets:[
+                {
+                label:"Confirmed Cases",
+                data:activeCases,
+                backgroundColor:"rgb(255, 148, 77)",
+                borderColor:"rgb(255, 194, 153)"
+                },
+                {
+                    label:"Recovered Cases",
+                    data:recovered,
+                    backgroundColor:"#9fff80",
+                    borderColor:"#00b33c"
+                },
+                {
+                    label:"Death Cases",
+                    data:deceased,
+                    backgroundColor:"#ff0000",
+                    borderColor:"#ff8080"
+                },
+            ]
+        },
+        options:{}
+    })
+})
 
+                   
 })
